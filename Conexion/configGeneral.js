49e7f9b4ed2c9bdfb5a5612d59f012d091b3e095
Jvc <jhordyvc11>
2021-01-09 08:24:46 -0500

    .gitignore funciona

--- Conexion/configGeneral.js
+++
@@ -1,9 +0,0 @@
-
-let servidor=8888;
-let routeImagen='../public/imagenes';
-let routeImagenGet='../archivo/foto';
-
-module.exports.servidor=servidor;
-module.exports.imagenGet=routeImagenGet;
-module.exports.imagen=routeImagen;
-   
